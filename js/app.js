776bfdec663bcd6151d33ee71add5484cbdeda88
lili hualtibamba <lilihperezz@gmail.com>
2016-09-09 11:51:26 -0500

    objecto

---
+++ js/app.js
@@ -0,0 +1,46 @@
+
+window.addEventListener("load", function() {
+   var button = document.getElementById("calcular");
+   button.addEventListener("click", function() {
+       var nombre= document.getElementById("nombre").value;
+	   var edad= parseInt(document.getElementById("edad").value);
+	   var sexo= document.getElementById("sexo").value;
+
+		function Persona(nombre,edad,sexo){
+			this.nombre=nombre;
+			this.edad = edad;
+			this.sexo= sexo;
+			this.mayorDeEdad =(this.edad >=18);
+			this.presentacion = function()
+			{
+				if (this.mayorDeEdad){
+					return "Hola, mi nombre es " + this.nombre +  ", tengo "+ this.edad  + " años y soy mayor de edad.";
+				}else{
+					return "Hola, mi nombre es " + this.nombre +  ", tengo "+ this.edad  + " años y soy menor de edad.";
+				}
+			}
+		}
+
+			var persona1 = new Persona(nombre,edad,sexo);
+				document.write(persona1.presentacion());            
+	});
+});
+		
+
+
+		/*this.saludar=function(){
+				return = "Hola" + this.nombre;
+	 	};
+	}
+/*	var persona1 = new Persona("Ivan",22)
+});
+
+	
+saludar: function(){
+    console.log("Hola" + this.nombre);
+}
+};
+
+mayorDeEdad = (edad >=18) ?true :false;
+*/
+ 
